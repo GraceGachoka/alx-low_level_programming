bf91621eb22192d8aa5b647ac63fc58758b504ff
GraceGachoka <graciegachoka@gmail.com>
2023-03-16 12:19:07 -0700

    Compile only

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * @void: No Argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+
+	printf("The size of integers is %d\n", sizeof(int));
+	printf("The size of character is %d\n", sizeof(char));
+	printf("The size of float is %d\n", sizeof(float));
+	 
+	return (0);
+}
