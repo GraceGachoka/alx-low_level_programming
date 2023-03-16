8bf3cc5ca3a26fe0bf452295c3de6172d64a1420
GraceGachoka <graciegachoka@gmail.com>
2023-03-16 13:18:33 -0700

    program that prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,8 @@
  */
 int main(void)
 {
-
-
-	printf("The size of integers is %d\n", sizeof(int));
-	printf("The size of character is %d\n", sizeof(char));
-	printf("The size of float is %d\n", sizeof(float));
-	 
+	printf("The size of integers is %lu bytes\n", sizeof(int));
+	printf("The size of character is %lu bytes\n", sizeof(char));
+	printf("The size of float is %lu bytes\n", sizeof(float));
 	return (0);
 }
