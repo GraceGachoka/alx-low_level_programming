22d74c02c5b9cc544ae19a98833275dd8c34c700
GraceGachoka <graciegachoka@gmail.com>
2023-03-21 10:17:31 -0700

    MY functions

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - function name
+ * @c/ character o print
+ *
+ * Return: on Succes 1
+ *
+ *  On error, -1 is returned
+ */
+int _putchar(char c);
+{
+	return (write(1, &c, 1));
+}
