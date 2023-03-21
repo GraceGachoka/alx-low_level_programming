3b73d704b65640da707914ad0cfe42a634de3368
GraceGachoka <graciegachoka@gmail.com>
2023-03-21 10:46:37 -0700

    Print lowercase letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - Print lowercase letters
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	for (i = 97; i < 123; i++;)
+	{
+		putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
