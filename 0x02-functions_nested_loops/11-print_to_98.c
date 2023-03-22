d8091eba9dea4a6272aa698fb17f345452644df5
GraceGachoka <graciegachoka@gmail.com>
2023-03-22 15:11:43 -0700

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,54 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n >= 0 && n <= 98)
+	if (n >= 0 && n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			if (i != 98)
+			{
 			printf("%d", i);
 			printf(",");
 			printf(" ");
+			}
+			else if (i == 98)
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+			printf("%d", i);
+			printf(",");
+			printf(" ");
+			}
+			else if (i == 98)
+			{
+				printf("%d", i);
+			}
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+			printf("%d", i);
+			printf(",");
+			printf(" ");
+			}
+			else if (i == 98)
+			{
+				printf("%d", i);
+			}
 		}
 		printf("\n");
 	}
@@ -27,24 +68,5 @@ void print_to_98(int n)
 		printf("%d", n);
 		printf("\n");
 	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			printf(",");
-			printf(" ");
-		}
-		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			printf(",");
-			printf(" ");
-		}
-		printf("\n");
-	}
 }
+
