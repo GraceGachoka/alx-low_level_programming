d39c9d33611f15b53c3451eceff71479d788d0cf
GraceGachoka <graciegachoka@gmail.com>
2023-03-22 14:35:40 -0700

    Print natural numbers upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,36 +11,40 @@
 void print_to_98(int n)
 {
 	int i;
-	int j;
 
-	if (n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i != 98)
-			{
-				printf(",\t");
-				printf("%d, ", i);
-			}
-			else if (i == 98)
-			{
-				printf("%d\n", i);
-			}
+			printf("%d", i);
+			printf(",");
+			printf(" ");
 		}
+		printf("\n");
 	}
-       	else if (n >= 98)
+	else if (n == 98)
 	{
-		for (j = n; j >= 98; j--)
+		printf("%d", n);
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
 		{
-			if (j != 98)
-			{
-				printf(",\t");
-				printf("%d, ", j);
-			}
-			else if (j == 98)
-			{
-				printf("%d\n", j);
-			}
+			printf("%d", i);
+			printf(",");
+			printf(" ");
 		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			printf(",");
+			printf(" ");
+		}
+		printf("\n");
 	}
 }
