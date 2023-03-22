82d175ac2146bb45661f2e9c3825dc5eec557431
GraceGachoka <graciegachoka@gmail.com>
2023-03-22 11:01:26 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Print fibonacci numbers
+ *
+ * Retrn: Fibonacci
+ */
+int main(void)
+{
+	int n;
+	unsigned long a = 0;
+	unsigned long b = 1;
+	unsigned long sum;
+
+	for (n = 0; n < 50; n++)
+	{
+		sum = a+b;
+		printf("%lu",sum);
+		printf(",");
+		printf("\t");
+		a = b;
+		b = sum;
+		if (n == 49)
+		{
+			printf(",");
+			printf("\n");
+		}
+	}
+}
