6708944eb5273d43cb95ab44e794ca6ae40e5327
GraceGachoka <graciegachoka@gmail.com>
2023-03-23 07:49:13 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  more_numbers - Prints 0 to 10 10 times
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y < 10)
+			{
+				_putchar((y % 10) + '0');
+			}
+			else if (y >= 10)
+			{
+				_putchar((y / 10) + '0');
+				_putchar((y % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
