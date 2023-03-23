8d5ff5e02689fa97fd3949e74aefb926bc85bc0f
GraceGachoka <graciegachoka@gmail.com>
2023-03-23 10:57:45 -0700

    Fizz Buzz TESt

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,8 +15,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
+			if (i != 100)
+			{
 			printf("FizzBuzz");
 			printf(" ");
+			}
+			else if (i == 100)
+			{
+				printf("FizzBuzz");
+			}
 		}
 		else if (i % 3 == 0)
 		{
