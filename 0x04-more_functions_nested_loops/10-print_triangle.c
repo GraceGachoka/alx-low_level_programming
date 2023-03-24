981dc0902918f10d9dd4b8627d5f2e5a86b54dd1
GraceGachoka <graciegachoka@gmail.com>
2023-03-24 06:19:44 -0700

    Mirror trianle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a triangle
+ * @size: Parameter
+ *
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (x = 1; x <= size; x++)
+	{
+		for (y = x; y <= size; y++)
+		{
+			_putchar(' ');
+		}
+		for (y = 1; y <= x; y++)
+		{
+			_putchar ('#');
+		}
+		_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
