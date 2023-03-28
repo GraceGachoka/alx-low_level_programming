e13c6e3c0a889b08c4f48a7fe72395b53335bdf8
GraceGachoka <graciegachoka@gmail.com>
2023-03-28 12:30:19 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * puts_half - Prints half of a string
+ * @str: characters
+ *
+ */
+void puts_half(char *str)
+{
+	int a;
+	int x;
+	int y;
+
+	y = 0;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+		y++;
+		x = y/2;
+		if (y % 2 == 1)
+		{
+			x = (y + 1)/2;
+		}
+	}
+		for (a = x; str[a] != 0; a++)
+		{
+			_putchar(str[a]);
+		}
+		_putchar('\n');
+}
+
+
