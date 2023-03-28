5e3253130b149f7f0dfa2410ddedfc196a49102e
GraceGachoka <graciegachoka@gmail.com>
2023-03-28 14:54:29 -0700

    crack me

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generate a random pssword
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int pass[100];
+	int a;
+	int b;
+	int s;
+
+	s = 0;
+	srand(time(NULL));
+	for (a = 0; a < 100; a++)
+	{
+		pass[a] = rand() % 78;
+		s = (pass[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - s) - '0' < 78)
+		{
+			b = 2772 - s - '0';
+			s = s + b;
+			putchar(b + '0');
+			break;
+		}
+	}
+	return (0);
+}
