8b5b002f4e3f321724cb0674da586df0107124a6
GraceGachoka <graciegachoka@gmail.com>
2023-03-28 09:22:00 -0700

    strln function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Prints the length of a string
+ * @s: String
+ *
+ * Return: 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
