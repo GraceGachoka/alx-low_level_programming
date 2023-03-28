f30840569b8de0415458c30d77dca436ec2008f1
GraceGachoka <graciegachoka@gmail.com>
2023-03-28 15:28:45 -0700

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
+
 
 /**
  * print_rev - Print a stringin reverse
@@ -9,7 +8,19 @@
  */
 void print_rev(char *s)
 {
-	strrev(*s);
-	_putchar(*s);
+	int x;
+	int y = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		y++;
+	}
+	s--;
+	for (x = y; x > 0; x--)
+	{
+		_putchar(*s);
+		s--;
+	}
 	_putchar('\n');
 }
