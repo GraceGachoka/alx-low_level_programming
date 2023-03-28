b3fa9a02fb09e3ce121059f4ace280c72fae23d2
GraceGachoka <graciegachoka@gmail.com>
2023-03-28 08:13:51 -0700

    takes a pointer to an int as parameter and updates the value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Rest value stored in pointer to 98
+ * @*n: Pointer
+ * @n: Value stored in pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Intro to pointers strings and arrays
