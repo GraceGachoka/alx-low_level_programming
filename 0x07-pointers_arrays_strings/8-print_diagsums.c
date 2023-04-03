b052aa1bde6258a658a89504f8e46e93acfa0bdd
GraceGachoka <graciegachoka@gmail.com>
2023-04-03 12:43:12 -0700

    sums up diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Sums yp diagonal
+ * @a: array
+ * @size: size of array
+ * Return: 0 success
+ */
+void print_diagsums(int *a, int size)
+{
+	int S1;
+	int S2;
+	int x;
+
+	S1 = 0;
+	S2 = 0;
+	for (x = 0; x < size; x++)
+	{
+		S1 += a[x * size + x];
+	}
+	for (x = size; x >= 0; x--)
+	{
+		S2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d,%d\n", S1, S2);
+}
